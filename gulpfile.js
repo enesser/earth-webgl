faf93bd7862dc9bcd44c63d6f7ab432dd78e75ff
Eric J Nesser <enesser@gmail.com>
2016-01-04 17:11:28 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  sass = require('gulp-ruby-sass');
+
+gulp.task('sass', function () {
+  return sass('./public/css/**/*.scss')
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'bin/www',
+    ext: 'js handlebars coffee',
+    stdout: false
+  }).on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
+
+gulp.task('default', [
+  'sass',
+  'develop',
+  'watch'
+]);
