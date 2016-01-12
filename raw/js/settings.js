fbfd28e838a290920e998c90d6e9943f32e214c5
Eric J Nesser <enesser@gmail.com>
2016-01-11 21:40:30 -0600

    Refactored URL parser, added ability to show/hide UI with query string

--- raw/js/settings.js
+++ raw/js/settings.js
@@ -5,7 +5,8 @@
 var EarthWebGLDemo = EarthWebGLDemo || {};
 
 /**
- * Represents configurable settings for the demo.
+ * Create an instance of configurable settings for the demo.
+ * You can allow or disallow a GUI for these settings with the query string "ui=true/false", ie: http://localhost:3000?ui=false
  * @return {object} Scene settings
  */
 EarthWebGLDemo.settings = function() {
@@ -16,110 +17,115 @@ EarthWebGLDemo.settings = function() {
      */
     const sunColor = '#ffeedd',
 
-    /**
-     * Default sun intensity
-     * @type {Number}
-     */
-    sunIntensity = 5,
+        /**
+         * Default sun intensity
+         * @type {Number}
+         */
+        sunIntensity = 5,
 
-    /**
-     * Default ambient light color
-     * @type {String}
-     */
-    ambientLight = '#555555',
+        /**
+         * Default ambient light color
+         * @type {String}
+         */
+        ambientLight = '#555555',
 
-    /**
-     * Atmosphere is visible?
-     * @type {Boolean}
-     */
-    atmsophereVisible = true,
+        /**
+         * Atmosphere is visible?
+         * @type {Boolean}
+         */
+        atmsophereVisible = true,
 
-    /**
-     * Atmosphere color
-     * @type {String}
-     */
-    atmosphereColor = '#001ea3',
+        /**
+         * Atmosphere color
+         * @type {String}
+         */
+        atmosphereColor = '#001ea3',
 
-    /**
-     * Atmosphere opacity
-     * @type {Number}
-     */
-    atmopshereOpacity = 0.3,
+        /**
+         * Atmosphere opacity
+         * @type {Number}
+         */
+        atmopshereOpacity = 0.3,
 
-    /**
-     * Show atmosphere wireframe
-     * @type {Boolean}
-     */
-    atmosphereWireframe = false,
+        /**
+         * Show atmosphere wireframe
+         * @type {Boolean}
+         */
+        atmosphereWireframe = false,
 
-    /**
-     * Clouds are visible?
-     * @type {Boolean}
-     */
-    cloudsVisible = true,
+        /**
+         * Clouds are visible?
+         * @type {Boolean}
+         */
+        cloudsVisible = true,
 
-    /**
-     * Clouds opacity
-     * @type {Number}
-     */
-    cloudsOpacity = 0.6,
+        /**
+         * Clouds opacity
+         * @type {Number}
+         */
+        cloudsOpacity = 0.6,
 
-    /**
-     * Show clouds wireframe
-     * @type {Boolean}
-     */
-    cloudsWireframe = false,
+        /**
+         * Show clouds wireframe
+         * @type {Boolean}
+         */
+        cloudsWireframe = false,
 
-    /**
-     * Auto-rotate clouds around the planet?
-     * @type {Boolean}
-     */
-    cloudsRotate = true,
+        /**
+         * Auto-rotate clouds around the planet?
+         * @type {Boolean}
+         */
+        cloudsRotate = true,
 
-    /**
-     * Speed of automatic cloud rotation (can be negative for backwards)
-     * @type {Number}
-     */
-    cloudsVelocity = 0.002,
+        /**
+         * Speed of automatic cloud rotation (can be negative for backwards)
+         * @type {Number}
+         */
+        cloudsVelocity = 0.002,
 
-    /**
-     * Terrain is visible?
-     * @type {Boolean}
-     */
-    terrainVisible = true,
+        /**
+         * Terrain is visible?
+         * @type {Boolean}
+         */
+        terrainVisible = true,
 
-    /**
-     * Terrain bump height scale (altitude of bump map, can be negative for inset)
-     * @type {Number}
-     */
-    terrainBumpScale = 0.04,
+        /**
+         * Terrain bump height scale (altitude of bump map, can be negative for inset)
+         * @type {Number}
+         */
+        terrainBumpScale = 0.04,
 
-    /**
-     * Show terrain wireframe
-     * @type {Boolean}
-     */
-    terrainWireframe = false,
+        /**
+         * Show terrain wireframe
+         * @type {Boolean}
+         */
+        terrainWireframe = false,
 
-    /**
-     * Auto-rotate terrain?
-     * @type {Boolean}
-     */
-    terrainRotate = true,
+        /**
+         * Auto-rotate terrain?
+         * @type {Boolean}
+         */
+        terrainRotate = true,
 
-    /**
-     * Speed of automatic terrain rotation (can be negative for backwards)
-     * @type {Number}
-     */
-    terrainVelocity = 0.001;
+        /**
+         * Speed of automatic terrain rotation (can be negative for backwards)
+         * @type {Number}
+         */
+        terrainVelocity = 0.001;
 
-    let gui = new dat.GUI({ width: 360 });
-    gui.close();
+    let showUi = EarthWebGLDemo.urlParser.getQueryValueByKey('ui') !== 'false';
+    let gui = null;
+
+    if (showUi) {
+        gui = new dat.GUI({ width: 360 });
+        gui.close();
+    }
 
     /**
      * Settings schema business object
      * @type {Object}
      */
-    let settingsSchema = { 
+    let settingsSchema = {
         /**
          * Reset settings to default
          */
@@ -142,42 +148,54 @@ EarthWebGLDemo.settings = function() {
             this.terrainRotate = terrainRotate;
             this.terrainVelocity = terrainVelocity;
 
-            for (let folder in gui.__folders) {
-                for (let i in gui.__folders[folder].__controllers) {                    
-                    gui.__folders[folder].__controllers[i].updateDisplay();                    
+            if (showUi) {
+                for (let folder in gui.__folders) {
+                    for (let i in gui.__folders[folder].__controllers) {
+                        gui.__folders[folder].__controllers[i].updateDisplay();
+                    }
                 }
             }
         }
     };
 
+    /**
+     * Bind settings schema object to dat.gui interface.
+     * @param  {object} settingsSchema
+     */
+    function bindSettingsSchemaToUi(settingsSchema) {
+        let sunFolder = gui.addFolder('Sun');
+        sunFolder.addColor(settingsSchema, 'sunColor');
+        sunFolder.add(settingsSchema, 'sunIntensity').min(-10).max(50);
+        sunFolder.addColor(settingsSchema, 'ambientLight');
+
+        let atmosphereFolder = gui.addFolder('Atmosphere');
+        atmosphereFolder.add(settingsSchema, 'atmosphereVisible');
+        atmosphereFolder.addColor(settingsSchema, 'atmosphereColor');
+        atmosphereFolder.add(settingsSchema, 'atmosphereOpacity').min(0).max(1);
+        atmosphereFolder.add(settingsSchema, 'atmosphereWireframe');
+
+        let cloudsFolder = gui.addFolder('Clouds');
+        cloudsFolder.add(settingsSchema, 'cloudsVisible');
+        cloudsFolder.add(settingsSchema, 'cloudsOpacity').min(0).max(1);
+        cloudsFolder.add(settingsSchema, 'cloudsWireframe');
+        cloudsFolder.add(settingsSchema, 'cloudsRotate');
+        cloudsFolder.add(settingsSchema, 'cloudsVelocity').min(-0.01).max(0.01);
+
+        let terrainFolder = gui.addFolder('Terrain');
+        terrainFolder.add(settingsSchema, 'terrainVisible');
+        terrainFolder.add(settingsSchema, 'terrainBumpScale').min(-10).max(10);
+        terrainFolder.add(settingsSchema, 'terrainWireframe');
+        terrainFolder.add(settingsSchema, 'terrainRotate');
+        terrainFolder.add(settingsSchema, 'terrainVelocity').min(-0.01).max(0.01);
+
+        gui.add(settingsSchema, 'reset');
+    }
+
     settingsSchema.reset();
 
-    let sunFolder = gui.addFolder('Sun');
-    sunFolder.addColor(settingsSchema, 'sunColor'); 
-    sunFolder.add(settingsSchema, 'sunIntensity').min(-10).max(50); 
-    sunFolder.addColor(settingsSchema, 'ambientLight'); 
+    if (showUi) {
+        bindSettingsSchemaToUi(settingsSchema);
+    }
 
-    let atmosphereFolder = gui.addFolder('Atmosphere');
-    atmosphereFolder.add(settingsSchema, 'atmosphereVisible'); 
-    atmosphereFolder.addColor(settingsSchema, 'atmosphereColor'); 
-    atmosphereFolder.add(settingsSchema, 'atmosphereOpacity').min(0).max(1); 
-    atmosphereFolder.add(settingsSchema, 'atmosphereWireframe'); 
-
-    let cloudsFolder = gui.addFolder('Clouds');
-    cloudsFolder.add(settingsSchema, 'cloudsVisible'); 
-    cloudsFolder.add(settingsSchema, 'cloudsOpacity').min(0).max(1); 
-    cloudsFolder.add(settingsSchema, 'cloudsWireframe');
-    cloudsFolder.add(settingsSchema, 'cloudsRotate'); 
-    cloudsFolder.add(settingsSchema, 'cloudsVelocity').min(-0.01).max(0.01);
-
-    let terrainFolder = gui.addFolder('Terrain');
-    terrainFolder.add(settingsSchema, 'terrainVisible'); 
-    terrainFolder.add(settingsSchema, 'terrainBumpScale').min(-10).max(10); 
-    terrainFolder.add(settingsSchema, 'terrainWireframe'); 
-    terrainFolder.add(settingsSchema, 'terrainRotate'); 
-    terrainFolder.add(settingsSchema, 'terrainVelocity').min(-0.01).max(0.01);
-
-    gui.add(settingsSchema, 'reset');
-
-    return settingsSchema;   
+    return settingsSchema;
 };
---
+++ raw/js/urlParser.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* global dat: true */
+
+var EarthWebGLDemo = EarthWebGLDemo || {};
+
+/**
+ * URL parser, currently supports getting query string values by key.
+ * @return {object} URL parser
+ */
+EarthWebGLDemo.urlParser = {
+
+    /**
+     * Get query string value by key.
+     * @param  {string} key
+     * @return {string} value
+     */
+    getQueryValueByKey: (key) => {
+
+        let query = window.location.search.substring(1);
+        let vars = query.split('&');
+
+        for (let i = 0; i < vars.length; i++) {
+            let pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == key) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+    }
+};
