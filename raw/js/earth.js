8dda3cf73cb8c17cb2a9c347d71e84f72ad9fa41
Eric J Nesser <enesser@gmail.com>
2016-01-12 00:13:32 -0600

    Refactoring of earth object

---
+++ raw/js/earth.js
@@ -0,0 +1,143 @@
+'use strict';
+
+var EarthWebGLDemo = EarthWebGLDemo || {};
+
+/**
+ * Create an instance of earth, represents the ThreeJS model with meshes and materials.
+ * You can load high or low quality textures with the query string "quality=high/low", ie: http://localhost:3000?quality=low
+ * @return {object} earth model
+ */
+EarthWebGLDemo.earth = function() {
+
+    const pathEarthModelLow = 'models/earth/low/earth.obj';
+    const pathEarthModelHigh = 'models/earth/high/earth.obj';
+    const pathEarthModelMaterialLow = 'models/earth/low/earth.mtl';
+    const pathEarthModelMaterialHigh = 'models/earth/high/earth.mtl';
+
+    /**
+     * Represents the ThreeJS model of earth with meshes and materials.
+     * @type {Object}
+     */
+    let earthObject = {
+
+        /**
+         * Are textures and materials loaded yet?
+         * @type {Boolean}
+         */
+        isLoaded: false,
+
+        /**
+         * Texture and materials loading progress
+         * @type {Number}
+         */
+        percentLoaded: 0,
+
+        /**
+         * Group containing the meshes that comprise atmosphere, clouds, terrain
+         * @type {[type]}
+         */
+        meshGroup: null,
+
+        /**
+         * Mesh representing the atmosphere
+         * @type {[type]}
+         */
+        atmosphereMesh: null,
+
+        /**
+         * Material on the atmosphere mesh
+         * @type {[type]}
+         */
+        atmosphereMaterial: null,
+
+        /**
+         * Mesh representing the clouds
+         * @type {[type]}
+         */
+        cloudsMesh: null,
+
+        /**
+         * Material on the clouds mesh
+         * @type {[type]}
+         */
+        cloudsMaterial: null,
+
+        /**
+         * Mesh representing terrain
+         * @type {[type]}
+         */
+        terrainMesh: null,
+
+        /**
+         * Material on the terrain mesh
+         * @type {[type]}
+         */
+        terrainMaterial: null
+    };
+
+    /**
+     * Load materials and textures, then map them to the object.
+     * @param  {object} earthObject Target object to map.
+     */
+    function mapMaterialsTexturesToObject(earthObject) {
+
+        let lowQuality = EarthWebGLDemo.urlParser.getQueryValueByKey('quality') === 'low';
+        let earthPath, earthMaterialPath;
+
+        if (lowQuality) {
+            earthPath = pathEarthModelLow;
+            earthMaterialPath = pathEarthModelMaterialLow;
+        } else {
+            earthPath = pathEarthModelHigh;
+            earthMaterialPath = pathEarthModelMaterialHigh;
+        }
+
+        let loader = new THREE.OBJMTLLoader();
+        loader.load(earthPath, earthMaterialPath, (o) => {
+
+                //map imported object (OBJ) to our wrapper
+                earthObject.meshGroup = o;
+                earthObject.atmosphereMesh = o.children[0];
+                earthObject.atmosphereMaterial = earthObject.atmosphereMesh.children[1].material;
+                earthObject.cloudsMesh = o.children[1];
+                earthObject.cloudsMaterial = earthObject.cloudsMesh.children[1].material;
+                earthObject.terrainMesh = o.children[2];
+                earthObject.terrainMaterial = earthObject.terrainMesh.children[1].material;
+
+                //make adjustments to the imported model that were lost in the import process
+                earthObject.atmosphereMaterial.depthWrite = false;
+                earthObject.cloudsMaterial.blending = THREE.AdditiveBlending;
+                earthObject.cloudsMaterial.castShadow = true;
+                earthObject.terrainMaterial.receiveShadow = true;
+
+                //make sure the model isn't shiny otherwise it reflects light unrealistically
+                earthObject.atmosphereMaterial.shininess = 0;
+                earthObject.cloudsMaterial.shininess = 0;
+                earthObject.terrainMaterial.shininess = 0;
+
+                //flag model as loaded after all adjustments
+                earthObject.isLoaded = true;
+            },
+
+            /**
+             * Successful load
+             * @param  {object} xhr XmlHttpRequest
+             */
+            (xhr) => {
+                if (xhr.lengthComputable) {
+                    earthObject.percentLoaded = xhr.loaded / xhr.total * 100;
+                }
+            },
+
+            /**
+             * Error loading earth materials and textures
+             * @param  {object} xhr XmlHttpRequest
+             */
+            (xhr) => {
+                console.error('Error loading earth materials and textures', xhr);
+            });
+    }
+
+    mapMaterialsTexturesToObject(earthObject);
+    return earthObject;
+};
--- raw/js/main.js
+++ raw/js/main.js
@@ -4,7 +4,7 @@
 
 ((window) => {
     //window.onload = ()=> {
-      //  console.log('test...');
+    //  console.log('test...');
 
     //};
 })();
@@ -13,159 +13,109 @@
 
 window.onload = () => {
 
-let settings = EarthWebGLDemo.settings();
+    let settings = EarthWebGLDemo.settings();
 
-var container;
+    var container;
 
-var camera, scene, renderer;
+    var camera, scene, renderer;
 
-var mouseX = 0,
-mouseY = 0;
+    var mouseX = 0,
+        mouseY = 0;
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
 
-var obj = null;
-
-
-init();
-animate();
-
-var directionalLight;
-var directionalLightColor;
-var ambientLightColor;
-var ambientLight; 
-var atmosphereColor;
+    var obj = null;
 
 
-function init() {
+    init();
+    animate();
 
-    container = document.createElement('div');
-    document.body.appendChild(container);
-
-
-                // scene
-
-                scene = new THREE.Scene();
-
-                camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-                //camera.position.z = 100;
+    var directionalLight;
+    var directionalLightColor;
+    var ambientLightColor;
+    var ambientLight;
+    var atmosphereColor;
+    var earth;
 
 
-                //var ambient = new THREE.AmbientLight(0x444444);
-                ambientLightColor = new THREE.Color();
-                ambientLight = new THREE.AmbientLight(ambientLightColor);
-                //var ambient = new THREE.AmbientLight(0xFFFFFF);
-                scene.add(ambientLight);
+    function init() {
 
-                directionalLightColor = new THREE.Color();
-                directionalLight = new THREE.DirectionalLight(directionalLightColor);
-                directionalLight.castShadow = true;
-                //directionalLight.position.set(-200, 10, 10).normalize();
-                scene.add(directionalLight);
-
-                atmosphereColor = new THREE.Color();
-
-                // model
-
-                var onProgress = function(xhr) {
-                    if (xhr.lengthComputable) {
-                        var percentComplete = xhr.loaded / xhr.total * 100;
-                        console.log(Math.round(percentComplete, 2) + '% downloaded');
-                    }
-                };
-
-                var onError = function(xhr) {};
-
-                const pathEarthModelLow = 'models/earth/low/earth.obj';
-                const pathEarthModelHigh = 'models/earth/high/earth.obj';
-                const pathEarthModelMaterialLow = 'models/earth/low/earth.mtl';
-                const pathEarthModelMaterialHigh = 'models/earth/high/earth.mtl';
-
-                let earthPath = '';
-                let earthMaterialPath = '';
+        container = document.createElement('div');
+        document.body.appendChild(container);
 
 
-                function getQueryVariable(variable) {
-                    var query = window.location.search.substring(1);
-                    var vars = query.split('&');
-                    for (var i = 0; i < vars.length; i++) {
-                        var pair = vars[i].split('=');
-                        if (decodeURIComponent(pair[0]) == variable) {
-                            return decodeURIComponent(pair[1]);
-                        }
-                    }
-                }
+        // scene
+
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+        //camera.position.z = 100;
 
 
-                let quality = getQueryVariable('quality') === 'low' ? 'low' : 'high';
+        //var ambient = new THREE.AmbientLight(0x444444);
+        ambientLightColor = new THREE.Color();
+        ambientLight = new THREE.AmbientLight(ambientLightColor);
+        //var ambient = new THREE.AmbientLight(0xFFFFFF);
+        scene.add(ambientLight);
 
-                if (quality === 'low') {
-                    earthPath = pathEarthModelLow;
-                    earthMaterialPath = pathEarthModelMaterialLow;
-                } else {
-                    earthPath = pathEarthModelHigh;
-                    earthMaterialPath = pathEarthModelMaterialHigh;
-                }
+        directionalLightColor = new THREE.Color();
+        directionalLight = new THREE.DirectionalLight(directionalLightColor);
+        directionalLight.castShadow = true;
+        //directionalLight.position.set(-200, 10, 10).normalize();
+        scene.add(directionalLight);
 
-                var loader = new THREE.OBJMTLLoader();
-                loader.load(earthPath, earthMaterialPath, function(object) {
+        atmosphereColor = new THREE.Color();
 
-                    console.dir(object);
-                    obj = object;
-                    //obj.scale.x += 70;
-                    //obj.scale.y += 70;
-                    //obj.scale.z += 70;
-                    //object.position.y = -80;
-                    scene.add(object);
-
-                }, onProgress, onError);
-
-                //
-
-                renderer = new THREE.WebGLRenderer({
-                    alpha: true,
-                    antialias: true
-                });
-                renderer.setPixelRatio(window.devicePixelRatio);
-                renderer.setSize(window.innerWidth, window.innerHeight);
-                container.appendChild(renderer.domElement);
-
-                document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-                //
-
-                window.addEventListener('resize', onWindowResize, false);
-
-            }
-
-            function onWindowResize() {
-
-                windowHalfX = window.innerWidth / 2;
-                windowHalfY = window.innerHeight / 2;
-
-                camera.aspect = window.innerWidth / window.innerHeight;
-                camera.updateProjectionMatrix();
-
-                renderer.setSize(window.innerWidth, window.innerHeight);
-
-            }
+        earth = EarthWebGLDemo.earth();
+        earth.isAddedToScene = false;
 
 
-        var stars = new THREE.Geometry();
-    for (var i=0; i<10000; i++) {
-      stars.vertices.push(new THREE.Vector3(
-        //1e3 * Math.random() - 5e2,
-        //1e3 * Math.random() - 5e2,
-        //-1e2
-        //1e3 * Math.random() - 5e2,
-        //1e3 * Math.random() - 5e2,
-        //-1e2
         //
-        Math.random() * 2000 - 1000,
-        Math.random() * 2000 - 1000,
-        Math.random() * 2000 - 1000
-      ));
+
+        renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            antialias: true
+        });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(renderer.domElement);
+
+        document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+        //
+
+        window.addEventListener('resize', onWindowResize, false);
+
+    }
+
+    function onWindowResize() {
+
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+    }
+
+
+    var stars = new THREE.Geometry();
+    for (var i = 0; i < 10000; i++) {
+        stars.vertices.push(new THREE.Vector3(
+            //1e3 * Math.random() - 5e2,
+            //1e3 * Math.random() - 5e2,
+            //-1e2
+            //1e3 * Math.random() - 5e2,
+            //1e3 * Math.random() - 5e2,
+            //-1e2
+            //
+            Math.random() * 2000 - 1000,
+            Math.random() * 2000 - 1000,
+            Math.random() * 2000 - 1000
+        ));
     }
 
     var star_stuff = new THREE.PointsMaterial();
@@ -173,112 +123,79 @@ function init() {
     var star_system = new THREE.Points(stars, star_stuff);
     scene.add(star_system);
 
-            function onDocumentMouseMove(event) {
+    function onDocumentMouseMove(event) {
 
-                if (event.buttons) {
-                mouseX = (event.clientX - windowHalfX) / 2;
-                mouseY = (event.clientY - windowHalfY) / 2;
-            }
-
-
-            }
-
-             //
-
-             function animate() {
-
-                requestAnimationFrame(animate);
-                render();
-
-            }
-
-            function render() {
-
-                directionalLightColor.setStyle(settings.sunColor);
-                directionalLight.color = directionalLightColor;
-
-                ambientLightColor.setStyle(settings.ambientLight);
-                ambientLight.color = ambientLightColor;
-
-                if (settings) {
-                camera.position.set(5.25, 0, 0);
-
-                directionalLight.position.set(-1, 1, -10);
-                directionalLight.intensity = settings.sunIntensity;
-
-                if (obj) {
-            }
-            }
-
-                    camera.position.x += ( mouseX - camera.position.x ) * 0.005;
-        camera.position.y += ( - mouseY - camera.position.y ) * 0.005;
-        camera.lookAt( scene.position );
+        if (event.buttons) {
+            mouseX = (event.clientX - windowHalfX) / 2;
+            mouseY = (event.clientY - windowHalfY) / 2;
+        }
 
 
-                if (obj) {
+    }
 
-                    //obj.position.set(0, 0, -100);
+    //
 
-                    //obj.position.x = 0;
-                    //obj.position.y = 0;
-                    //obj.position.z = 0;
-                    //
-                    //
+    function animate() {
 
-                    atmosphereColor.setStyle(settings.atmosphereColor);
-                    obj.children[0].visible = settings.atmosphereVisible;
-                    obj.children[0].children[1].material.color = atmosphereColor; 
-                    obj.children[0].children[1].material.wireframe = settings.atmosphereWireframe;
+        requestAnimationFrame(animate);
+        render();
 
+    }
 
-                    obj.children[0].children[1].material.opacity = settings.atmosphereOpacity;
-                    obj.children[0].children[1].material.depthWrite = false; //fix atmospehre
-                    obj.children[1].children[1].material.opacity = settings.cloudsOpacity; //fix clouds
-                    //obj.children[1].children[1].material.opacity = 1; //fix clouds
-                    obj.children[1].children[1].material.blending = THREE.AdditiveBlending;
+    function render() {
 
-                    obj.children[1].children[1].material.wireframe = settings.cloudsWireframe;
+        if (earth.isLoaded && !earth.isAddedToScene) {
+            scene.add(earth.meshGroup);
+        }
 
-                    obj.children[1].children[1].material.visible = settings.cloudsVisible;
+        directionalLightColor.setStyle(settings.sunColor);
+        directionalLight.color = directionalLightColor;
 
+        ambientLightColor.setStyle(settings.ambientLight);
+        ambientLight.color = ambientLightColor;
 
-                    obj.children[1].children[1].material.shininess = 0; //fix clouds
-                    obj.children[2].children[1].material.shininess = 0; //fix clouds
-                    obj.children[0].children[1].material.shininess = 0; //fix clouds
+        if (settings) {
+            camera.position.set(5.25, 0, 0);
 
+            directionalLight.position.set(-1, 1, -10);
+            directionalLight.intensity = settings.sunIntensity;
 
-                    obj.children[1].children[0].material.shininess = 0; //fix clouds
-                    obj.children[2].children[0].material.shininess = 0; //fix clouds
-                    obj.children[0].children[0].material.shininess = 0; //fix clouds
+            if (earth.isLoaded) {
 
+                atmosphereColor.setStyle(settings.atmosphereColor);
 
+                earth.atmosphereMesh.visible = settings.atmosphereVisible;
+                earth.atmosphereMaterial.color = atmosphereColor;
+                earth.atmosphereMaterial.opacity = settings.atmosphereOpacity;
+                earth.atmosphereMaterial.wireframe = settings.atmosphereWireframe;
 
-                    obj.children[1].castShadow = true;
-                    obj.children[2].receiveShadow = true;
-                    obj.children[2].visible = settings.terrainVisible;
-                    obj.children[2].children[1].material.bumpScale = settings.terrainBumpScale;
-                    obj.children[2].children[1].material.wireframe = settings.terrainWireframe;
+                earth.cloudsMesh.visible = settings.cloudsVisible;
+                earth.cloudsMaterial.opacity = settings.cloudsOpacity;
+                earth.cloudsMaterial.wireframe = settings.cloudsWireframe;
 
-                    //obj.children[0].visible = false;
-                    //
-                    if (settings.cloudsRotate) {
-                        obj.children[1].rotation.y += settings.cloudsVelocity;
-                    }
+                earth.terrainMesh.visible = settings.terrainVisible;
+                earth.terrainMaterial.bumpScale = settings.terrainBumpScale;
+                earth.terrainMaterial.wireframe = settings.terrainWireframe;
 
-                    if (settings.terrainRotate) {
-                        obj.children[2].rotation.y += settings.terrainVelocity;
-                    }
+                if (settings.cloudsRotate) {
+                    earth.cloudsMesh.rotation.y += settings.cloudsVelocity;
                 }
 
-
-                if (obj) {
-                    //camera.lookAt(obj.position);
+                if (settings.terrainRotate) {
+                    earth.terrainMesh.rotation.y += settings.terrainVelocity;
                 }
 
-                renderer.render(scene, camera);
-
-
+                if (!earth.isAddedToScene) {
+                    scene.add(earth.meshGroup);
+                    earth.isAddedToScene = true;
+                }
             }
 
+        }
 
-        };
+        camera.position.x += (mouseX - camera.position.x) * 0.005;
+        camera.position.y += (-mouseY - camera.position.y) * 0.005;
+        camera.lookAt(scene.position);
+        renderer.render(scene, camera);
+    }
+};
